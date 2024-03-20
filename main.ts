a6a39efdd5ac61e2dd63389c7a518f69f2a5d880
Ghulam Muneer uddin <gmugsk@gmail.com>
2024-03-20 16:00:44 +0500

    assignment 05

---
+++ main.ts
@@ -0,0 +1,11 @@
+/* 
+Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+*/
+
+const famousPerson: string = "Hazrat Muhammad SAW famous Quote, Seek knowledge from the cradle to the grave."
+
+const message: string = `${famousPerson}` 
+
+console.log(famousPerson)
+
+
